c1ef2cd51717a6450f0926dfe52541e316e4627d
202121375@sangmyung.kr <202121375@sangmyung.kr>
2023-08-01 13:21:38 +0900

    project1

---
+++ summer_2023/fifo.v
@@ -0,0 +1,16 @@
+module fifo(
+    clk,
+    n_rst,
+    tx_data,
+    rx_data,
+    rx_fifo,
+    result
+);
+
+    input clk;
+    input n_rst;
+    output tx_data;
+    output rx_fifo;
+
+    
+endmodule
